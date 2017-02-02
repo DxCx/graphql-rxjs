c29ca3563b17098fc7f3382303f6934400086a6c
Hagai Cohen <hagai.co@gmail.com>
2017-02-02 20:02:06 +0200

    chore(build): clean old test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-graphql = require('graphql');
-//require('./register');
-//console.log(graphql.executeReactive);
-pkg = require('./index');
-console.log(pkg.GraphQLSchema === graphql.GraphQLSchema);
