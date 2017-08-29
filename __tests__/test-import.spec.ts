3c3ecb46cda13ba1af529ce3ab360ec92b0baae8
Hagai Cohen <hagai.co@gmail.com>
2017-08-29 19:38:05 +0300

    fix: combine async iterator did wrong array overwrites

--- __tests__/test-import.spec.ts
+++ __tests__/test-import.spec.ts
@@ -753,4 +753,95 @@ describe('import-tests Rx Engines', () => {
         expect(values).toMatchSnapshot();
     });
   });
+
+  it("@live with infinate + static result sibling", () => {
+    const obs = [];
+
+    const typeDefs = `
+      type OtherType {
+        value: String
+      }
+
+      type FinalObject {
+        root: Int
+        value: Int
+      }
+
+      type Nested {
+        root: Int
+        inf: OtherType
+        liveInt: FinalObject
+      }
+
+      type Query {
+        nestedType: [Nested]
+      }
+      `;
+
+    const trackObservable = (observable) => new Observable((observer) => {
+      const sub = observable.subscribe(observer);
+      const id = obs.length;
+      obs.push(true);
+
+      return () => {
+        obs[id] = false;
+        sub.unsubscribe();
+      };
+    });
+
+    const resolvers = {
+      OtherType: {
+        value: (root) => Promise.resolve(root.value),
+      },
+      Nested: {
+        root(root) {
+          return root;
+        },
+        inf(root) {
+          return trackObservable(new Observable((observer) => {
+            observer.next({ value: root });
+            // No Complete.
+          }));
+        },
+        liveInt(root, args, ctx) {
+          return trackObservable(Observable.interval(20).map((i) => ({ root: root, value: i * root })));
+        }
+      },
+      Query: {
+        nestedType(root, args, ctx) {
+          return trackObservable(new Observable((observer) => {
+            observer.next([2]);
+          }));
+        },
+      },
+    };
+
+    const scheme = makeExecutableSchema({typeDefs, resolvers});
+    prepareSchema(scheme);
+    const query = `query mixedLive {
+      nestedType @live {
+        root
+        inf {
+          value
+        }
+        liveInt {
+          root
+          value
+        }
+      }
+    }`;
+
+    return graphqlRx(scheme, query)
+      .map((v) => v.data.nestedType)
+      .take(10)
+      .toArray().toPromise().then((values) => {
+        // If something left it means unsubscribe was not called for it.
+        expect(obs
+          .map((v, i) => ({ v, i }))
+          .filter((m) => m.v)
+          .map((m) => m.i)
+        ).toHaveLength(0);
+        expect(values).toMatchSnapshot();
+    });
+  });
 });
